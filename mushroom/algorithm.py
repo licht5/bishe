9c6595e3ad03e4ec82f5a69129f58cbc2577f83f
licht5 <1961807207@qq.com>
2019-02-21 14:06:23 +0800

    Added functionality: store data in  excel sheets

--- mushroom/algorithm.py
+++ mushroom/algorithm.py
@@ -15,7 +15,9 @@ from sklearn import tree
 from sklearn import svm
 import general.pretreatmnet as  pretreatmnet
 import general.unbalanceRate as unbalanceRate
-rate_x, rate_y = 7, 1
+import os
+from xlutils.copy import copy
+rate_x, rate_y = 13, 1
 test_rate = 0.4
 att_type="num"
 att_add="a"
@@ -30,6 +32,32 @@ test_filename="../dataFile/test.csv"
 train_data,train_target= shujuchuli.getdata(train_filename, att_add,att_type)
 ts_data,ts_target= shujuchuli.getdata(test_filename, att_add,att_type)
 
+# ******************************* execel *******************************
+excel_filename="../dataFile/data.xls"
+alg_name=["svm","NB","deTree","adaBoost","rdForest"]
+def makeExists(acc,pre,rec,f1,auc):
+    workbook = xlwt.Workbook()
+    for index in range(len(alg_name)):
+        table = workbook.add_sheet(alg_name[index])
+        table.write(0, 0, "acc")
+        table.write(0, 1, "pre")
+        table.write(0, 2, "rec")
+        table.write(0, 3, "f1")
+        table.write(0, 4, "auc")
+        if index==0:
+            table.write(1, 0, acc)
+            table.write(1, 1, pre)
+            table.write(1, 2, rec)
+            table.write(1, 3, f1)
+            table.write(1, 4, auc)
+
+    workbook.save(excel_filename)
+
+
+
+
+
+
 # *******************************统计不平衡下的数量*******************************
 shuju_1=sum(train_target)
 print("种类1的训练样本:"+str(shuju_1))
@@ -42,19 +70,33 @@ print("种类2的测试样本:"+str(len(ts_target)-shuju_2))
 
 
 # ******************************* 显示评价指标 *******************************
-def showResult(result):
+def showResult(result,i):
     acc= metrics.accuracy_score(ts_target, result)
-    print("acc",acc)
     pre= metrics.accuracy_score(ts_target, result)
-    print("pre", pre)
     rec=metrics.recall_score(ts_target, result)
-    print("rec", rec)
     f1= metrics.f1_score(ts_target, result)
-    print("f1:",f1)
     auc=metrics.roc_auc_score(ts_target, result)
+    if os.path.exists(excel_filename):
+        excel_data=xlrd.open_workbook(excel_filename)
+        table=excel_data.sheet_by_name(alg_name[i])
+        rows=table.nrows
+        newWB = copy(excel_data)
+        sheet = newWB.get_sheet(i)
+        sheet.write(rows, 0, acc)
+        sheet.write(rows, 1, pre)
+        sheet.write(rows, 2, rec)
+        sheet.write(rows, 3, f1)
+        sheet.write(rows, 4, auc)
+        newWB.save(excel_filename)
+    else:
+        makeExists(acc,pre,rec,f1,auc)
+
+
+    print("acc", acc)
+    print("pre", pre)
+    print("rec", rec)
+    print("f1:", f1)
     print("auc:", auc)
-
-
     print("\n")
 
 # *******************************支持向量机 70/345*******************************
@@ -63,7 +105,7 @@ def svcFunc():
     clf=clf.fit(train_data,train_target)
     result=clf.predict(ts_data)
     print("svm:")
-    showResult(result)
+    showResult(result,0)
 
 
 #*******************************高斯朴素贝叶斯 77/345*******************************
@@ -72,7 +114,7 @@ def gsNB():
     clf = gnb.fit(train_data, train_target)
     result = gnb.predict(ts_data)
     print("guassNB:")
-    showResult(result)
+    showResult(result,1)
 
 
 
@@ -82,7 +124,7 @@ def deTree():
     clf = clf.fit(train_data, train_target)
     result = clf.predict(ts_data)
     print("DecisionTree:")
-    showResult(result)
+    showResult(result,2)
 
 
 #******************************* adaboost 的集成方法 *******************************
@@ -91,7 +133,7 @@ def adaBoost():
     clf = clf.fit(train_data, train_target)
     result = clf.predict(ts_data)
     print("adaboost")
-    showResult(result)
+    showResult(result,3)
 
 
 # *******************************随机森林的集成方法  n_estimators为5时，准确率最高 *******************************
@@ -100,7 +142,7 @@ def rdForest():
     clf = clf.fit(train_data, train_target)
     result = clf.predict(ts_data)
     print("RandomForest:")
-    showResult(result)
+    showResult(result,4)
 
 
 
